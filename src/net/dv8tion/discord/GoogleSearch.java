306f78dd81ce2ff6b55d55dd23367a682ce54b1a
DV8FromTheWorld <keeneraustin@yahoo.com>
2015-11-07 05:34:02 +0900

    Moved the code that searches Google out of SearchCommand and into it's own class. This is the first part of a code refactor to move many of the commands in SearchCommand to their own classes. This will be done to specialize the returned results.

---
+++ src/net/dv8tion/discord/GoogleSearch.java
@@ -0,0 +1,62 @@
+package net.dv8tion.discord;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class GoogleSearch
+{
+    private JsonArray results;
+    private String suggestedString;
+
+    public GoogleSearch(String terms)
+    {
+        this.suggestedString = performSearch(terms.replaceAll(" ", "+"));
+    }
+
+    public String getSuggestedReturn()
+    {
+        return suggestedString;
+    }
+
+    private String performSearch(String terms) {
+        try {
+            StringBuilder searchURLString = new StringBuilder();
+            searchURLString.append("https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=");
+            searchURLString.append(terms);
+
+            URL searchURL = new URL(searchURLString.toString());
+            URLConnection conn = searchURL.openConnection();
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 RavenBot/2.0");
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder json = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                json.append(line).append("\n");
+            }
+            in.close();
+            JsonElement element = new JsonParser().parse(json.toString());
+            results = element.getAsJsonObject().getAsJsonObject("responseData").getAsJsonArray("results");
+            JsonObject output = results.get(0).getAsJsonObject();
+            String title = StringEscapeUtils.unescapeJava(StringEscapeUtils.unescapeHtml4(output.get("titleNoFormatting").toString().replaceAll("\"", "")));
+            String content = StringEscapeUtils.unescapeJava(StringEscapeUtils.unescapeHtml4(output.get("content").toString().replaceAll("\\s+", " ").replaceAll("\\<.*?>", "").replaceAll("\"", "")));
+            String url = StringEscapeUtils.unescapeJava(output.get("url").toString().replaceAll("\"", ""));
+            return URLDecoder.decode(url, "UTF-8") + " - *" + title + "*: \"" + content + "\"";
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/net/dv8tion/discord/commands/SearchCommand.java
+++ src/net/dv8tion/discord/commands/SearchCommand.java
@@ -1,23 +1,13 @@
 package net.dv8tion.discord.commands;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLDecoder;
 import java.util.Arrays;
 import java.util.List;
 
 import me.itsghost.jdiscord.events.UserChatEvent;
 import me.itsghost.jdiscord.message.MessageBuilder;
+import net.dv8tion.discord.GoogleSearch;
 
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 public class SearchCommand extends Command
 {
@@ -44,42 +34,16 @@ public class SearchCommand extends Command
 			default:
 				return;
 		}
+
+		GoogleSearch search = new GoogleSearch(
+		        String.format("%s+%s",
+		                StringUtils.join(splitMessage, "+", 1, splitMessage.length),
+		                filter));
+
 		e.getGroup().sendMessage(new MessageBuilder()
             .addUserTag(e.getUser(), e.getGroup())
-            .addString(": " + performSearch(filter, StringUtils.join(splitMessage, " ", 1, splitMessage.length)))
+            .addString(": " + search.getSuggestedReturn())
             .build());
-	}
-
-	private String performSearch(String filter, String terms) {
-		try {
-			StringBuilder searchURLString = new StringBuilder();
-			searchURLString.append("https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=");
-			if (filter != null) {
-				searchURLString.append(filter).append("+");
-			}
-			searchURLString.append(terms.replace(" ", "+"));
-			URL searchURL = new URL(searchURLString.toString());
-			URLConnection conn = searchURL.openConnection();
-			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 RavenBot/2.0");
-			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			StringBuilder json = new StringBuilder();
-			String line;
-			while ((line = in.readLine()) != null) {
-				json.append(line).append("\n");
-			}
-			in.close();
-			JsonElement element = new JsonParser().parse(json.toString());
-			JsonObject output = element.getAsJsonObject().getAsJsonObject("responseData").getAsJsonArray("results").get(0).getAsJsonObject();
-			String title = StringEscapeUtils.unescapeJava(StringEscapeUtils.unescapeHtml4(output.get("titleNoFormatting").toString().replaceAll("\"", "")));
-			String content = StringEscapeUtils.unescapeJava(StringEscapeUtils.unescapeHtml4(output.get("content").toString().replaceAll("\\s+", " ").replaceAll("\\<.*?>", "").replaceAll("\"", "")));
-			String url = StringEscapeUtils.unescapeJava(output.get("url").toString().replaceAll("\"", ""));
-			return URLDecoder.decode(url, "UTF-8") + " - *" + title + "*: \"" + content + "\"";
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		return null;
 	}
 
     @Override
