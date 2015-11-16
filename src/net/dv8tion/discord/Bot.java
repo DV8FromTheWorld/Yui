0ed045eacc52750e8a898c880a89138fa10f48c8
DV8FromTheWorld <keeneraustin@yahoo.com>
2015-11-17 05:29:08 +0900

    Added relauncher system to the startup of the Bot. Now automatically relaunches the jar using -Dfile.encoding=UTF-8 if it wasn't already in UTF-8 mode. Need to add option to config to specify path for 'java' variable instead of relying on it being installed to %path%.

--- src/net/dv8tion/discord/Bot.java
+++ src/net/dv8tion/discord/Bot.java
@@ -1,5 +1,9 @@
 package net.dv8tion.discord;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+
 import me.itsghost.jdiscord.DiscordAPI;
 import me.itsghost.jdiscord.DiscordBuilder;
 import me.itsghost.jdiscord.event.EventManager;
@@ -16,7 +20,32 @@ import net.dv8tion.discord.commands.TestCommand;
 public class Bot
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException, InterruptedException
+    {
+        if (System.getProperty("file.encoding").equals("UTF-8"))
+        {
+            setupBot();
+        }
+        else
+        {
+            System.out.println("BotLauncher: We are not running in UTF-8 mode! This is a problem!");
+            System.out.println("BotLauncher: Relaunching in UTF-8 mode using -Dfile.encoding=UTF-8");
+
+            //Gets the path of the currently running Jar file
+            String path = Bot.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            String decodedPath = URLDecoder.decode(path, "UTF-8");
+            File f = new File(decodedPath); //We use File so that when we send the path to the ProcessBuilder, we will be using the proper System path formatting.
+
+            //Relaunches the bot using UTF-8 mode.
+            ProcessBuilder processBuilder =
+                    new ProcessBuilder("java", "-Dfile.encoding=UTF-8", "-jar", f.getAbsolutePath());
+            processBuilder.inheritIO(); //Tells the new process to use the same command line as this one.
+            Process process = processBuilder.start();
+            process.waitFor();  //We wait here until the actual bot stops. We do this so that we can keep using the same command line.
+        }
+    }
+
+    private static void setupBot()
     {
         try
         {
@@ -45,6 +74,5 @@ public class Bot
             System.out.println("We failed to connect to the Discord API. Do you have internet connection?");
             System.out.println("Also double-check your Config.json for possible mistakes.");
         }
-        System.out.println("Launcher: Leaving Main");
     }
 }
