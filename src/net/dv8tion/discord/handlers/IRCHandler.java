40fac8fea52ea9ba848c99d9cf67c0c81e5d89be
DV8FromTheWorld <keeneraustin@yahoo.com>
2015-12-04 02:49:44 +0900

    Modified the IRCHandler to use onMessage instead of onGenericMessage. Less trash messages.

--- src/net/dv8tion/discord/handlers/IRCHandler.java
+++ src/net/dv8tion/discord/handlers/IRCHandler.java
@@ -2,18 +2,17 @@ package net.dv8tion.discord.handlers;
 
 import java.io.IOException;
 
+import me.itsghost.jdiscord.event.EventListener;
+import me.itsghost.jdiscord.events.APILoadedEvent;
+import me.itsghost.jdiscord.events.UserChatEvent;
+import me.itsghost.jdiscord.talkable.Group;
 import net.dv8tion.discord.Bot;
 
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
 import org.pircbotx.exception.IrcException;
 import org.pircbotx.hooks.ListenerAdapter;
-import org.pircbotx.hooks.types.GenericMessageEvent;
-
-import me.itsghost.jdiscord.event.EventListener;
-import me.itsghost.jdiscord.events.APILoadedEvent;
-import me.itsghost.jdiscord.events.UserChatEvent;
-import me.itsghost.jdiscord.talkable.Group;
+import org.pircbotx.hooks.events.MessageEvent;
 
 public class IRCHandler implements EventListener
 {
@@ -49,12 +48,13 @@ public class IRCHandler implements EventListener
         .setName("Yui") //Set the nick of the bot. CHANGE IN YOUR CODE
         .setAutoNickChange(true)
         .setRealName("Yui-MHCP001")
+        .setMessageDelay(100)
         .setServer("irc.esper.net", 6667)
         .addAutoJoinChannel("#nubtards") //Join the official #pircbotx channel
         .addListener(new ListenerAdapter()
         {
             @Override
-            public void onGenericMessage(GenericMessageEvent event) {
+            public void onMessage(MessageEvent event) {
 //                System.out.print("I did a thing");
                 System.out.println("<" + event.getUser().getNick() + "> " + event.getMessage());
                 group.sendMessage("<" + event.getUser().getNick() + "> " + event.getMessage());
