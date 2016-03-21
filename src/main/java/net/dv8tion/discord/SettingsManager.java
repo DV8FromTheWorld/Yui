142a4a132e96a3f095d0970712a0949ba8d83159
Austin Keener <keeneraustin@yahoo.com>
2016-03-21 16:54:29 +0900

    Added Apache 2.0 License. Slightly import cleanup.

--- src/main/java/net/dv8tion/discord/SettingsManager.java
+++ src/main/java/net/dv8tion/discord/SettingsManager.java
@@ -1,6 +1,17 @@
 /**
- * This code came directly from Smbarbour's RavenBot.
- * https://github.com/MCUpdater/RavenBot/blob/master/src/main/java/org/mcupdater/ravenbot/SettingsManager.java
+ *     Copyright 2015-2016 Austin Keener
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package net.dv8tion.discord;
 
@@ -20,6 +31,10 @@ import net.dv8tion.discord.bridge.IrcConnectInfo;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+/**
+ * This code came directly from Smbarbour's RavenBot.
+ * https://github.com/MCUpdater/RavenBot/blob/master/src/main/java/org/mcupdater/ravenbot/SettingsManager.java
+ */
 public class SettingsManager {
     private static SettingsManager instance;
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
